d37e581d219ad6ce403d1fbae12d1205673c17d7
Ricardo <ricardommps@gmail.com>
2019-10-23 16:44:40 -0300

    Ajuste

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = require("./app");
 
 if (process.env.NODE_ENV !== "production" && dotenv.error) throw dotenv.error;
 
-app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${process.env.PORT}`));
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT}`));
